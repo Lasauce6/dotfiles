78656850e9303e9074100a1a216b1e83a2908b63
Raphael Baticle <rbaticle@made-f0Ar5s5.clusters.42paris.fr>
2024-11-20 10:28:15 +0100

    change cursor nvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -77,3 +77,6 @@ vim.cmd[[colorscheme cyberdream]]
 
 -- Autoupdate window on size change
 vim.api.nvim_command('autocmd VimResized * wincmd =')
+
+-- Change cursor color
+opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
