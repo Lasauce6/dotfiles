418f0945ad4f5c860d04030530ec15d96c180e8e
Raphael Baticle <rbaticle@made-f0Cr3s3.clusters.42paris.fr>
2024-11-28 15:21:53 +0100

    add keymap to toggle nvim-tree

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -80,3 +80,6 @@ vim.api.nvim_command('autocmd VimResized * wincmd =')
 
 -- Change cursor color
 opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+
+-- Keymap to toogle nvim-tree
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Toogle Nvim Tree' })
