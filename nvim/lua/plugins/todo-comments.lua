541d22e9c0e6b858067e7339f80011376a67d462
Raphael Baticle <rbaticle@bess-f2r2s18.clusters.42paris.fr>
2024-12-10 09:14:46 +0100

    todo nvim

---
+++ nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,20 @@
+return {
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local todo = require("todo-comments")
+
+		todo.setup({
+			-- list of named colors where we try to extract the guifg from the
+			-- list of highlight groups or use the hex color if hl not found as a fallback
+			colors = {
+				error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+				warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+				info = { "DiagnosticInfo", "#2563EB" },
+				hint = { "DiagnosticHint", "#10B981" },
+				default = { "Identifier", "#7C3AED" },
+				test = { "Identifier", "#FF00FF" },
+			},
+		})
+	end,
+}
